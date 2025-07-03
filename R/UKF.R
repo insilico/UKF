5fae8ad98171d5ca413f54b592e1f1dd441a872d
Selim Karaoglu <kenj.s.kar@hotmail.com>
2025-07-02 23:35:28 -0500

    Q and R

--- R/UKF.R
+++ R/UKF.R
@@ -20,59 +20,65 @@
 #' @param R covariance matrix of y state. initialized in UKF_blend and updated with sigma points as Pyy in current function.
 #' @param dt smaller time step size within dT
 #' @param dT time step size that comes from time series data step
+#' @param R_scale related to standard deviation of Gaussian measurement noise of ind variables. A number that must be specified by user.
+#' @param Q_scale related to standard deviation of process noise. Noise related to model parameters
 #' @param forcePositive logical, if TRUE, ensures all parameters stay positive
 #' @return list: xhat (augmented Kalman update), Pxx (augmented covariance at sigma points), and K (Kalman gain matrix)
 #' @examples
 #' Example
 #' @export
-UKF_dT <- function(t_dummy, ode_model, xhat, Pxx, y, N_p, N_y, R, dt, dT, forcePositive = FALSE) {
-    N_x <- N_p + N_y # number of augmented states
-    N_sigma <- 2*N_x # number sigma points
-    xsigma <- t(chol(N_x*Pxx, pivot=T))
-    # Unscented Sigma Points
-    Xa <- xhat %*%
-      matrix(rep(1,length=N_sigma),nrow=1,ncol=N_sigma) +
-      cbind(xsigma,-xsigma)
+UKF_dT <- function(t_dummy, ode_model, xhat, Pxx, y, N_p, N_y, R, dt, dT,
+                    R_scale, Q_scale, forcePositive = FALSE) {
+  N_x <- N_p + N_y # number of augmented states
+  N_sigma <- 2 * N_x # number sigma points
+  xsigma <- t(chol(N_x * Pxx, pivot = T))
+  # Unscented Sigma Points
+  Xa <- xhat %*%
+    matrix(rep(1, length = N_sigma), nrow = 1, ncol = N_sigma) +
+    cbind(xsigma, -xsigma)
 
-    #X <- kura_ode_model(t,N_p,Xa,dT)
-    X <- propagate_model(t_dummy,ode_model,dt,dT,N_p,Xa)
-    xtilde <- t(t(rowSums(X))/N_sigma)
+  # X <- kura_ode_model(t,N_p,Xa,dT)
+  X <- propagate_model(t_dummy, ode_model, dt, dT, N_p, Xa)
+  xtilde <- t(t(rowSums(X)) / N_sigma)
 
-    Pxx <- matrix(rep(0,length=N_x*N_x),nrow=N_x,ncol=N_x)
-    for(i in 1:N_sigma){
-      Pxx <- Pxx + ((X[,i] - xtilde) %*%
-                      t(X[,i] - xtilde))/N_sigma
-    }
-    # grab first N_y ind vars
-    # skip N_p params from augmented
-    # previously used multiple_param_obs_fct
-    Y <- X[(N_p+1):(N_p+N_y),]
-    ytilde <- t(t(rowSums(Y))/N_sigma)
+  Pxx <- matrix(rep(0, length = N_x * N_x), nrow = N_x, ncol = N_x)
+  for(i in 1:N_sigma){
+    Pxx <- Pxx + ((X[, i] - xtilde) %*% t(X[, i] - xtilde)) / N_sigma
+  }
 
-    Pyy <- R
-    for(i in 1:N_sigma){
-      Pyy <- Pyy + ((Y[,i] - ytilde) %*%
-                      t(Y[,i] - ytilde))/N_sigma
-    }
+  # Add process noise Q at each step (only to parameter block)
+  Q <- matrix(0, nrow = N_x, ncol = N_x)
+  Q[1:N_p, 1:N_p] <- Q_scale * diag(N_p)
+  Pxx <- Pxx + Q  # Add process noise to covariance
 
-    Pxy <- matrix(rep(0,length = N_x*N_y),nrow=N_x,ncol=N_y)
-    for(i in 1:N_sigma){
-      Pxy <- Pxy + ((X[,i] - xtilde) %*%
-                      t(Y[,i] - ytilde))/N_sigma
-    }
-    K <- Pxy %*% solve(Pyy)  # Kalman Gain Matrix
+  # grab first N_y ind vars
+  # skip N_p params from augmented
+  # previously used multiple_param_obs_fct
+  Y <- X[(N_p + 1):(N_p + N_y),]
+  ytilde <- t(t(rowSums(Y)) / N_sigma)
 
-    xhat <- xtilde + K %*% (y - ytilde)
-    
-    if (forcePositive) {
-      param_min <- 1e-8  # Smallest allowed value for parameters
-      xhat[1:N_p] <- pmax(param_min, xhat[1:N_p])  # Ensure all parameters stay positive
-    }
-    
-    Pxx <- Pxx - K %*% t(Pxy)
+  Pyy <- R
+  for(i in 1:N_sigma){
+    Pyy <- Pyy + ((Y[, i] - ytilde) %*% t(Y[, i] - ytilde)) / N_sigma
+  }
 
-    # xhat is Kalman augmented predcition
-    return(list(xhat = xhat, Pxx = Pxx, K = K))
+  Pxy <- matrix(rep(0, length = N_x * N_y), nrow = N_x, ncol = N_y)
+  for(i in 1:N_sigma){
+    Pxy <- Pxy + ((X[, i] - xtilde) %*% t(Y[, i] - ytilde)) / N_sigma
+  }
+  K <- Pxy %*% solve(Pyy)  # Kalman Gain Matrix
+
+  xhat <- xtilde + K %*% (y - ytilde)
+  
+  if (forcePositive) {
+    param_min <- 1e-8  # Smallest allowed value for parameters
+    xhat[1:N_p] <- pmax(param_min, xhat[1:N_p])  # Ensure positive
+  }
+  
+  Pxx <- Pxx - K %*% t(Pxy)
+
+  # xhat is Kalman augmented prediction
+  return(list(xhat = xhat, Pxx = Pxx, K = K))
 } # ENDFN UKF
 
 #' propagate_model
@@ -89,20 +95,20 @@ UKF_dT <- function(t_dummy, ode_model, xhat, Pxx, y, N_p, N_y, R, dt, dT, forceP
 #' @examples
 #' Example
 #' @export
-propagate_model <- function(t,ode_model,dt,dT,N_p,x){
-  #dt <- 0.1*dT
-  nn <- round(dT/dt)
-  p <- x[1:N_p,]  # grab parameters
-  y <- x[(N_p+1):length(x[,1]),]  # grab ind vars
+propagate_model <- function(t, ode_model, dt, dT, N_p, x) {
+  # dt <- 0.1*dT
+  nn <- round(dT / dt)
+  p <- x[1:N_p, ]  # grab parameters
+  y <- x[(N_p + 1):length(x[, 1]), ]  # grab ind vars
   # Runge-Kutta
   for(n in 1:nn){
-    k1 <- dt*ode_model(t,y,p)
-    k2 <- dt*ode_model(t,y + k1/2,p)
-    k3 <- dt*ode_model(t,y + k2/2,p)
-    k4 <- dt*ode_model(t,y + k3,p)
-    y <- y + k1/6 + k2/3 + k3/3 + k4/6
+    k1 <- dt * ode_model(t, y, p)
+    k2 <- dt * ode_model(t, y + k1 / 2, p)
+    k3 <- dt * ode_model(t, y + k2 / 2, p)
+    k4 <- dt * ode_model(t, y + k3, p)
+    y <- y + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
   }
-  r <- rbind(x[(1:N_p),],y) # returns new augmented
+  r <- rbind(x[(1:N_p), ], y) # returns new augmented
   return(r)
 } # ENDFN propagate_model
 
@@ -135,96 +141,97 @@ propagate_model <- function(t,ode_model,dt,dT,N_p,x){
 #' @param R_scale related to standard deviation of Gaussian measurement noise of ind variables. A number that must be specified by user.
 #' @param Q_scale related to standard deviation of process noise. Noise related to model parameters. User choice. Can it be 0?
 #' @param forcePositive logical, if TRUE, ensures all parameters stay positive
+#' @param seeded logical, if TRUE, sets seed for reproducibility
 #' @return list: param_est (N_p model parameter estiamtes after run through time series), xhat (augmented Kalman update), error (error from Pxx augmented covariance at sigma points), and chisq (chi-square goodness of fit of prediction to data)
 #' @examples
 #' Example
 #' @export
-UKF_blend <- function(t_dummy,ts_data,ode_model,N_p,N_y,param_guess,dt,dT,R_scale=0.3,Q_scale=0.015,forcePositive = FALSE){
-  time_points <- ts_data[,1]
+UKF_blend <- function(t_dummy, ts_data, ode_model, N_p, N_y,
+                      param_guess, dt, dT, R_scale = 0.3, Q_scale = 0.015,
+                      forcePositive = FALSE, seeded = FALSE) {
+  time_points <- ts_data[, 1]
   num_time <- length(time_points)
   N_x <- N_p + N_y
   # Modified the length of xhat to be N_x*num_time
-  xhat <- matrix(rep(0,length=N_x*num_time),nrow=N_x,ncol=num_time)
+  xhat <- matrix(rep(0, length = N_x * num_time), nrow = N_x, ncol = num_time)
   # intialize Pxx
   Pxx <- vector(mode = "list", length = num_time)
   for(i in 1:num_time){
-    Pxx[[i]] <- matrix(rep(0,length=N_x*N_x),nrow=N_x,ncol=N_x)
+    Pxx[[i]] <- matrix(rep(0, length = N_x * N_x), nrow = N_x, ncol = N_x)
   }
   # Modified the length of errors to be N_x*num_time
-  errors <- matrix(rep(0,length=N_x*num_time),
-                   nrow=N_x,ncol=num_time)
+  errors <- matrix(rep(0, length = N_x * num_time),
+                   nrow = N_x, ncol = num_time)
   # intialize Ks
   Ks <- vector(mode = "list", length = num_time)
   for(i in 1:num_time){
-    Ks[[i]] <- matrix(rep(0,length=N_x*N_y),nrow=N_x,ncol=N_y)
+    Ks[[i]] <- matrix(rep(0, length = N_x * N_y), nrow = N_x, ncol = N_y)
   }
 
   # intialize x augmented
   # z stacked on top of y
   # z is param guess repeated for all time points
   z <- t(t(param_guess)) %*%
-    matrix(rep(1,length=num_time),nrow=1,ncol=num_time)
+    matrix(rep(1, length = num_time), nrow = 1, ncol = num_time)
   # y0 is intial y unaugmented state
   # transpose makes each column y values that's num_time wide
-  y0 <- t(ts_data[,-1])
+  y0 <- t(ts_data[, -1])
   # 2nd and 3rd cols of dat, first col (-1 remove) is time
   # x columns are intial augmented states
-  x <- rbind(z,y0) # stack
-  xhat[,1] <- x[,1]  # right now xhat is all data
+  x <- rbind(z, y0) # stack
+  xhat[, 1] <- x[, 1]  # right now xhat is all data
   #Q <- 0.015
   #                [grab y rows from x augmented]
-  R <- (R_scale)^2*cov(t(x[(N_p+1):(N_y+N_p),])) # only y
-  Pxx[[1]] <- pracma::blkdiag(Q_scale*diag(N_p),R)
+  R <- (R_scale)^2 * cov(t(x[(N_p + 1):(N_y + N_p), ])) # only y
+  Pxx[[1]] <- pracma::blkdiag(Q_scale * diag(N_p), R)
   #max_steps <- 300
   #steps <- 0
   #convergence <- 1
   #param_array <- list()
   # ?while loop would start here for repeats through time series
-    # intial augmented x
-    # z is the parameter guesses repeated for all time values
-    # y0 is the observed data from the time series
-    #x <- rbind(z,y0)
-    #xhat[,1] <- x[,1]
-    set.seed(1)
-    #    grabbing y from x augmented
-    y <- x[(N_p+1):(N_y+N_p),] +
-      (pracma::sqrtm(R)$B) %*%  # adding noise
-      matrix(rnorm(2*num_time),nrow=2,ncol=num_time)
+  # intial augmented x
+  # z is the parameter guesses repeated for all time values
+  # y0 is the observed data from the time series
+  #x <- rbind(z,y0)
+  #xhat[,1] <- x[,1]
+  if (seeded) set.seed(1)
+  #    grabbing y from x augmented
+  y <- x[(N_p + 1):(N_y + N_p), ] + (pracma::sqrtm(R)$B) %*%
+    matrix(rnorm(2 * num_time), nrow = 2, ncol = num_time)
 
-    # Create intial dummy UKF_kstep as a default value
-    # because cholesky, and hence UKF_dT in for-loop,
-    # sometimes fail
-    UKF_kstep <- list(xhat = xhat[,1],
-                      Pxx = Pxx[[1]],
-                      K = Ks[[1]])
-    for(k in 2:num_time){
-      #t_k <- time_points[k-1]
-      # bam: added tryCatch because by chance chol will fail
-      # due to input matrix being non positive definite.
-      UKF_kstep <- tryCatch(
-        {
-          UKF_dT(t_dummy,ode_model,xhat[,k-1],Pxx[[k-1]],y[,k],
-                 N_p,N_y,R,dt,dT,forcePositive=forcePositive)
-        },
-        error=function(cond) {
-          message("By chance, matrix caused Cholesky to fail.")
-          message("Here's the original error message:")
-          message(cond)
-          message("Skipping this iteration.")
-          return(UKF_kstep)
-          # if error occurs, return previous out
-        }
-      ) # end tryCatch
-      xhat[,k] <- UKF_kstep$xhat
-      Pxx[[k]] <- UKF_kstep$Pxx
-      Ks[[k]] <- UKF_kstep$K
-      errors[,k] <- sqrt(diag(Pxx[[k]]))
+  # Create intial dummy UKF_kstep as a default value
+  # because cholesky, and hence UKF_dT in for-loop,
+  # sometimes fail
+  UKF_kstep <- list(xhat = xhat[, 1], Pxx = Pxx[[1]], K = Ks[[1]])
+  for(k in 2:num_time){
+    # t_k <- time_points[k-1]
+    # bam: added tryCatch because by chance chol will fail
+    # due to input matrix being non positive definite.
+    UKF_kstep <- tryCatch(
+      {
+        UKF_dT(t_dummy, ode_model, xhat[, k - 1], Pxx[[k - 1]], y[, k],
+                N_p, N_y, R, dt, dT,
+                R_scale, Q_scale, forcePositive = forcePositive)
+      },
+      error=function(cond) {
+        message("By chance, matrix caused Cholesky to fail.")
+        message("Here's the original error message:")
+        message(cond)
+        message("Skipping this iteration.")
+        return(UKF_kstep)
+        # if error occurs, return previous out
+      }
+    ) # end tryCatch
+    xhat[, k] <- UKF_kstep$xhat
+    Pxx[[k]] <- UKF_kstep$Pxx
+    Ks[[k]] <- UKF_kstep$K
+    errors[, k] <- sqrt(diag(Pxx[[k]]))
 
     } # end application of UKF blend to all time points
 
     #est <- t(xhat[(1:N_p),num_time])
     # parameters at last time point
-    param_estimated <- t(t(xhat[(1:N_p),num_time]))
+    param_estimated <- t(t(xhat[(1:N_p), num_time]))
     # bam: Maybe we wnat to divide by the abs average of
     # param_esimated to normalize
     #convergence <- sum(abs(param_estimated - param_guess))
@@ -237,13 +244,13 @@ UKF_blend <- function(t_dummy,ts_data,ode_model,N_p,N_y,param_guess,dt,dT,R_scal
     # this would end a while loop for repeats through ts
   chisq <- 0
   for(i in 1:N_y){
-    chisq <- chisq + (x[(N_p+i),] - xhat[(N_p+i),])^2
+    chisq <- chisq + (x[(N_p + i), ] - xhat[(N_p + i), ])^2
   }
   chisq <- mean(chisq)
-  error <- t(errors[(1:N_p),num_time])
+  error <- t(errors[(1:N_p), num_time])
 
-  return(list(param_est=param_estimated,
-              xhat=xhat,error=error,chisq=chisq))
+  return(list(param_est = param_estimated,
+              xhat = xhat, error = error, chisq = chisq))
 } # END FN UKF_blend
 
 #' optim_params
@@ -264,14 +271,17 @@ UKF_blend <- function(t_dummy,ts_data,ode_model,N_p,N_y,param_guess,dt,dT,R_scal
 #' @param N_y number of ind variables
 #' @param dt smaller time step size within dT for solving ode
 #' @param dT time step size that comes from time series data step
+#' @param R_scale related to standard deviation of Gaussian measurement noise of ind variables. A number that must be specified by user.
+#' @param Q_scale related to standard deviation of process noise. Noise related to model parameters
 #' @param forcePositive logical, if TRUE, ensures all parameters stay positive
 #' @return list: par (vector of N_p optimized parameters) and value (final chi-square goodness of fit to time series)
 #' @examples
 #' Example
 #' @export
-optim_params <- function(param_guess,method="L-BFGS-B",
-                         lower_lim,upper_lim,maxit,temp=20,
-                    t_dummy,ts_data,ode_model,N_p,N_y,dt,dT,forcePositive=FALSE){
+optim_params <- function(param_guess, method = "L-BFGS-B",
+                        lower_lim, upper_lim, maxit, temp = 20,
+                        t_dummy, ts_data, ode_model, N_p, N_y, dt, dT,
+                        R_scale, Q_scale, forcePositive = FALSE) {
   # Optimize the model parameters
   # if method=L-BFGS-B
   #     lower/upper constraints used, no maxit
@@ -295,23 +305,24 @@ optim_params <- function(param_guess,method="L-BFGS-B",
     # objective function
     # one full pass through the time series.
     # Assign the result of UKF_blend to ukf_obj in the parent environment
-    ukf_obj <<- UKF_blend(t_dummy,ts_data,
-                       ode_model,
-                       N_p,N_y,par_vec,dt,dT,forcePositive = forcePositive)
+    ukf_obj <<- UKF_blend(t_dummy, ts_data, ode_model,
+                          N_p, N_y, par_vec, dt, dT,
+                          R_scale, Q_scale, forcePositive = forcePositive)
     return(ukf_obj$chisq)
   }
   # from stats base library
-  if (method=="SANN"){
+  if (method == "SANN") {
     # simulated annealing
-  opt <- optim(param_guess,chisq_objective,method="SANN",
-               control = list(maxit = maxit, temp = temp))
-  } else{
+    opt <- optim(param_guess, chisq_objective, method = "SANN",
+                 control = list(maxit = maxit, temp = temp))
+  } else {
     # Broyden-Fletcher-Goldfarb-Shannon
-    opt <- optim(param_guess,chisq_objective,method="L-BFGS-B",
-                 lower=lower_lim,upper=upper_lim)
+    opt <- optim(param_guess, chisq_objective, method = "L-BFGS-B",
+                 lower = lower_lim, upper = upper_lim)
   }
   # Modified the return parameters, param_est and xhat added
-  return(list(par=opt$par, value=opt$value, param_est=opt$par, xhat=ukf_obj$xhat))
+  return(list(par = opt$par, value = opt$value,
+              param_est = opt$par, xhat = ukf_obj$xhat))
 }
 
 #' iterative_param_optim
@@ -332,28 +343,42 @@ optim_params <- function(param_guess,method="L-BFGS-B",
 #' @param dT time step size that comes from time series data step
 #' @param param_tol small tolerance for convergence of the parameters to be optimized
 #' @param MAXSTEPS max number of iterations through time series
+#' @param R_scale related to standard deviation of Gaussian measurement noise of ind variables. A number that must be specified by user.
+#' @param Q_scale related to standard deviation of process noise. Noise related to model parameters
+#' @param trace logical, if TRUE, returns the best step and chi-square value
 #' @param forcePositive logical, if TRUE, ensures all parameters stay positive
 #' @return list: par (vector of N_p optimized parameters) and value (final chi-square goodness of fit to time series)
 #' @examples
 #' Example
 #' @export
-iterative_param_optim <- function(param_guess,
-                                  t_dummy, ts_data, ode_model,
-                                  N_p, N_y, dt, dT,
-                                  param_tol = 0.01, MAXSTEPS = 30,
+iterative_param_optim <- function(param_guess, t_dummy, ts_data, ode_model,
+                                  N_p, N_y, dt, dT, param_tol = 0.01, MAXSTEPS = 30,
+                                  R_scale = 1, Q_scale = 1, trace = FALSE,
                                   forcePositive = FALSE) {
   done <- FALSE
   steps <- 0
   chisq_history <- numeric()  # Store chi-square values for each iteration
 
+  # Initialize best parameter tracking
+  best_param <- param_guess
+  best_chisq <- Inf
+  best_xhat <- NULL
+
   while (!done) {
     # One run through whole time series
-    ukf_run <- UKF_blend(t_dummy, ts_data,
-                         ode_model,
-                         N_p, N_y, param_guess, dt, dT, forcePositive = forcePositive)
+    ukf_run <- UKF_blend(t_dummy, ts_data, ode_model,
+                         N_p, N_y, param_guess, dt, dT,
+                         R_scale, Q_scale, forcePositive = forcePositive)
 
     param_new <- ukf_run$param_est
     chisq_history <- c(chisq_history, ukf_run$chisq)  # Append chi-square
+
+    # Track best parameters
+    if (ukf_run$chisq < best_chisq) {
+      best_chisq <- ukf_run$chisq
+      best_param <- param_new
+      best_xhat <- ukf_run$xhat
+    }
 
     steps <- steps + 1
     param_norm <- abs(sum(param_new - param_guess))
@@ -362,15 +387,27 @@ iterative_param_optim <- function(param_guess,
 
     param_guess <- param_new  # Update guess for next iteration
   }
-
-  # Return includes full chi-square trace
-  return(list(
-    par = param_new,
-    value = ukf_run$chisq,
-    param_norm = param_norm,
-    steps = steps,
-    param_est = param_new,
-    xhat = ukf_run$xhat,
-    chisq = chisq_history  # NEW: chi-square over iterations
-  ))
+  # If trace is TRUE, return the best step and chi-square value
+  if (trace) {
+    return(list(
+      par = best_param,
+      value = best_chisq,
+      param_norm = param_norm,
+      steps = steps,
+      param_est = best_param,
+      xhat = best_xhat,
+      chisq = chisq_history  # chi-square over iterations
+    ))
+  } else {
+    # Return includes full chi-square trace
+    return(list(
+      par = param_new,
+      value = ukf_run$chisq,
+      param_norm = param_norm,
+      steps = steps,
+      param_est = param_new,
+      xhat = ukf_run$xhat,
+      chisq = chisq_history  # NEW: chi-square over iterations
+    ))
+  }
 }
